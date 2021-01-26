ffecd3576268d87ad35e9e542f6922c269315909
Mathieu Valery <mathieuromainvalery@gmail.com>
2021-01-26 23:16:15 +0100

    add class active to button

--- src/containers/button.jsx
+++ src/containers/button.jsx
@@ -5,12 +5,17 @@ import { setWaveform } from '../actions';
 
 class Button extends Component {
     handleClick = () => {
-     this.props.setWaveform(this.props.name);
-    }
+        this.props.setWaveform(this.props.name);
+        let DOMElements = document.querySelectorAll(".button");
+        DOMElements.forEach(DOMElement => (DOMElement.classList.remove("active")));
+        let DOMElement = document.getElementById(this.props.name);
+        DOMElement.classList.add("active");
+
+}
     
     render() {
         return(
-        <div className="button" onClick={this.handleClick}>{this.props.name}</div>
+        <div className="button" id={this.props.name} onClick={this.handleClick}>{this.props.name}</div>
         )
     }
 }
